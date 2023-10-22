4a9c89ab6e7d97fe12cb61ca3de6dd7b94352df5
Khaif Mohamed <khaifmohamededava@gmail.com>
2023-10-22 21:56:39 +0530

    added shop page animation

--- src/components/Shop/ProductRow.jsx
+++ src/components/Shop/ProductRow.jsx
@@ -1,12 +1,18 @@
+import { motion } from "framer-motion";
 import ProductRowCard from "./ProductRowCard";
 
 export default function ProductRow() {
   return (
-    <div className="grid grid-cols-1 gap-2 py-2 md:gap-4 md:py-4">
+    <motion.div
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      transition={{ duration: 0.75 }}
+      className="grid grid-cols-1 gap-2 py-2 md:gap-4 md:py-4"
+    >
       <ProductRowCard />
       <ProductRowCard />
       <ProductRowCard />
       <ProductRowCard />
-    </div>
+    </motion.div>
   );
 }
--- src/components/Shop/ProductsGrid.jsx
+++ src/components/Shop/ProductsGrid.jsx
@@ -1,14 +1,20 @@
+import { motion } from "framer-motion";
 import ProductCard from "../Home/ProductCard";
 
 export default function ProductsGrid() {
   return (
-    <section className="grid grid-cols-2 py-6 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4">
-        <ProductCard />
-        <ProductCard /> 
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-    </section>
-  )
+    <motion.section
+      initial={{ x: "-100%" }}
+      animate={{ x: 0 }}
+      transition={{ duration: 0.75 }}
+      className="grid grid-cols-2 gap-4 py-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4"
+    >
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </motion.section>
+  );
 }
